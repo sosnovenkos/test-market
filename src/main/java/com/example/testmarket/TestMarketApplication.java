1a98f7839df228ea0ab7c792445502ea32fd9e0b
anton <antonstolyarov08@gmail.com>
2022-04-19 13:01:54 +0300

    spring web app minimum

---
+++ src/main/java/com/example/testmarket/TestMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.testmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMarketApplication.class, args);
+	}
+
+}
