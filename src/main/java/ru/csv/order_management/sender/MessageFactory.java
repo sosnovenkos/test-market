b7a823e74210a967ea63c3759e292c209b87b828
anton <antonstolyarov08@gmail.com>
2022-05-26 17:52:36 +0300

    upgrade DbEntityOrder, createMessageForChooseDate in MessageFactory, handleChooseTimeCommand in OrderCommandService

--- src/main/java/ru/csv/order_management/sender/MessageFactory.java
+++ src/main/java/ru/csv/order_management/sender/MessageFactory.java
@@ -11,11 +11,16 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMar
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import ru.csv.order_management.domain.command.ChooseDateCommand;
+import ru.csv.order_management.domain.command.ChooseTimeCommand;
 import ru.csv.order_management.domain.context.*;
 
+import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @Slf4j
 @Service
@@ -257,5 +262,68 @@ public class MessageFactory {
                 .chatId(context.command.chatId)
                 .text(context.item.getName() + " " + context.item.getWeight() + " " + context.item.getPrice() + " руб.")
                 .build());
+    }
+
+    public List<SendMessage> createMessageForChooseDate(ChooseDateCommand command, OffsetDateTime date){
+        log.info("createMessageForChooseDate");
+        SendMessage sendMessage = new SendMessage();
+
+        sendMessage.setChatId(command.chatId);
+        sendMessage.setText("Выберите желаемую дату");
+        List<SendMessage> sendMessageList = new ArrayList<>();
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton(
+                    date.plusDays(i+1).getDayOfWeek().getDisplayName(TextStyle.FULL,
+                            Locale.getDefault())
+                            + " " + date.plusDays(i+1).getDayOfMonth()
+                            + " " + date.plusDays(i+1).getMonth().getDisplayName(TextStyle.FULL,
+                            Locale.getDefault())
+                            + " " + date.plusDays(i+1).getYear()
+            );
+            var cbd = "CHOOSE_TIME:" + date.plusDays(i+1).toInstant().toEpochMilli() + ":" + date.plusDays(i+1).getDayOfWeek().getValue();
+            log.info("CHOOSE_TIME:" + date.plusDays(i+1).toInstant().toEpochMilli()+ ":" + date.plusDays(i+1).getDayOfWeek().getValue());
+            inlineKeyboardButton.setCallbackData(cbd);
+            inlineKeyboardButton.getSwitchInlineQuery();
+            List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
+            keyboardButtonsRow1.add(inlineKeyboardButton);
+            rowList.add(keyboardButtonsRow1);
+        }
+        inlineKeyboardMarkup.setKeyboard(rowList);
+        sendMessage.setReplyMarkup(inlineKeyboardMarkup);
+        sendMessageList.add(sendMessage);
+        log.info("sendMessageList size" + sendMessageList.size());
+        return sendMessageList;
+    }
+
+    public List<SendMessage> createMessageForChooseTime(ChooseTimeCommand command, List<DbEntityTimeslot> timeslots){
+        log.info("createMessageForChooseTime");
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(command.chatId);
+        sendMessage.setText(command.getDate().getDayOfWeek().getDisplayName(TextStyle.FULL,
+                Locale.getDefault()) + " " + command.getDate().getDayOfMonth() + " " + command.getDate().getMonth().getDisplayName(TextStyle.FULL,
+                Locale.getDefault()) + "\n" + "Выберите время");
+        List<SendMessage> sendMessageList = new ArrayList<>();
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        for (int i = 0; i < timeslots.size(); i++) {
+            InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton(
+                    timeslots.get(i).getTimeslot()
+            );
+            var cbd = "ADD_TO_CART_FOR_ENTRY:" + command.getDate().toString().split("T")[0] + ":" + timeslots.get(i).getTimeslot() + ":" + timeslots.get(i).getId();
+            log.info("ADD_TO_CART_FOR_ENTRY:" + command.getDate().toString().split("T")[0] + ":" + timeslots.get(i).getTimeslot() + ":" + timeslots.get(i).getId());
+
+            inlineKeyboardButton.setCallbackData(cbd);
+            inlineKeyboardButton.getSwitchInlineQuery();
+            List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
+            keyboardButtonsRow1.add(inlineKeyboardButton);
+            rowList.add(keyboardButtonsRow1);
+        }
+        inlineKeyboardMarkup.setKeyboard(rowList);
+        sendMessage.setReplyMarkup(inlineKeyboardMarkup);
+        sendMessageList.add(sendMessage);
+        log.info("sendMessageList size" + sendMessageList.size());
+        return sendMessageList;
     }
 }
