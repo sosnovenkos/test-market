f0834a720af2a73574c458ccca15b0de3a417e90
anton <antonstolyarov08@gmail.com>
2022-05-26 18:08:48 +0300

    upgrade DbEntityOrder, createMessageForChooseDate in MessageFactory, handleChooseTimeCommand in OrderCommandService

--- src/main/java/ru/csv/order_management/store/entity/Timeslot.java
+++ src/main/java/ru/csv/order_management/store/entity/Timeslot.java
@@ -1,4 +1,47 @@
 package ru.csv.order_management.store.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+
+@Data
+@Entity
+@Table(name = "timeslot")
 public class Timeslot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
+    private long id;
+
+    @Column (name = "timeslots")
+    private String timeslot;
+
+    @Column(name = "parent_id")
+    private Long parentId;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTimeslot() {
+        return timeslot;
+    }
+
+    public void setTimeslot(String timeslot) {
+        this.timeslot = timeslot;
+    }
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
 }
